3ee3d66a1e46e82674c9bfcae1d6fe2ca38f3603
Sijmen Mulder <ik@sjmulder.nl>
2014-03-16 11:41:58 +0100

    Fix jshint errors in sample.js

--- sample/sample.js
+++ sample/sample.js
@@ -1,11 +1,15 @@
+'use strict';
+
 var fs = require('fs'),
 	nbt = require('../nbt');
 
 fs.readFile('bigtest.nbt.gz', function(error, data) {
-	if (error) throw error;
+	if (error) {
+		throw error;
+	}
 
 	nbt.parse(data, function(error, result) {
-    	console.log(result.Level.stringTest);
-    	console.log(result.Level['nested compound test']);
+		console.log(result.Level.stringTest);
+		console.log(result.Level['nested compound test']);
 	});
 });
