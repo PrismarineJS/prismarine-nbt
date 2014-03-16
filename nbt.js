d42a2aebcf65017a55748b2a827298237fc548ba
Sijmen Mulder <ik@sjmulder.nl>
2014-03-16 11:37:22 +0100

    Quote tagTypes keys

--- nbt.js
+++ nbt.js
@@ -16,18 +16,18 @@
 
 
 	var tagTypes = {
-		end: 0,
-		byte: 1,
-		short: 2,
-		int: 3,
-		long: 4,
-		float: 5,
-		double: 6,
-		byteArray: 7,
-		string: 8,
-		list: 9,
-		compound: 10,
-		intArray: 11
+		'end': 0,
+		'byte': 1,
+		'short': 2,
+		'int': 3,
+		'long': 4,
+		'float': 5,
+		'double': 6,
+		'byteArray': 7,
+		'string': 8,
+		'list': 9,
+		'compound': 10,
+		'intArray': 11
 	};
 
 	var tagTypeNames = new (function() {
