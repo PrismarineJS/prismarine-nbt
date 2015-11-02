42acccdbc5a76edec8ba4d1bf617df04f91679b0
Romain Beaumont <romain.rom1@gmail.com>
2015-11-02 18:25:15 +0100

    reimplement using protodef : fix #6
    * write a nbt.json to define the protocol : using mapper type and a string prefixed by short
    * use it with protodef to read and write nbt
    * replace the previous implementation while keeping the same API
    * rewrite the tests in a Data Driven style
    * splits the tests in 2 files : one for the datatypes tests, one for the overall parsing and serializing of nbt
    * add API and badges to the README

--- nbt.js
+++ nbt.js
@@ -1,264 +1,43 @@
-/*
-	NBT.js - a JavaScript parser for NBT archives
-	by Sijmen Mulder
+var zlib = require('zlib');
 
-	I, the copyright holder of this work, hereby release it into the public
-	domain. This applies worldwide.
+var hasGzipHeader = function(data){
+  var result=true;
+  if(data[0]!=0x1f) result=false;
+  if(data[1]!=0x8b) result=false;
+  return result;
+};
 
-	In case this is not legally possible: I grant anyone the right to use this
-	work for any purpose, without any conditions, unless such conditions are
-	required by law.
-*/
+var ProtoDef=require("protodef").ProtoDef;
+var proto=new ProtoDef();
 
-(function() {
-	'use strict';
+proto.addType("compound",require("./compound").compound);
+proto.addTypes(require("./nbt.json"));
 
-	var nbt = this;
-	var zlib = require('zlib');
+function writeUncompressed(value) {
+  return proto.createPacketBuffer("nbt",value);
+}
 
-	nbt.tagTypes = {
-		'end': 0,
-		'byte': 1,
-		'short': 2,
-		'int': 3,
-		'long': 4,
-		'float': 5,
-		'double': 6,
-		'byteArray': 7,
-		'string': 8,
-		'list': 9,
-		'compound': 10,
-		'intArray': 11
-	};
+function parseUncompressed(data) {
+  return proto.parsePacketBuffer("nbt",data).data;
+}
 
-	nbt.tagTypeNames = {};
-	(function() {
-		for (var typeName in nbt.tagTypes) {
-			if (nbt.tagTypes.hasOwnProperty(typeName)) {
-				nbt.tagTypeNames[nbt.tagTypes[typeName]] = typeName;
-			}
-		}
-	})();
+function parse(data, callback) {
+  if (hasGzipHeader(data)) {
+    zlib.gunzip(data, function(error, uncompressed) {
+      if (error) {
+        callback(error, data);
+      } else {
+        callback(null, parseUncompressed(uncompressed));
+      }
+    });
+  } else {
+    callback(null, parseUncompressed(data));
+  }
+}
 
-	var hasGzipHeader = function(data){
-		var result=true;
-		if(data[0]!=0x1f) result=false;
-		if(data[1]!=0x8b) result=false;
-		return result;
-	}
-
-    nbt.Writer = function() {
-		this.buffer = new Buffer(0);
-		var _offset = 0;
-		Object.defineProperty(this, "offset", {
-			get: function() { return _offset; },
-			set: function(newval) {
-				_offset = newval;
-				var newBuf = new Buffer(_offset);
-				this.buffer.copy(newBuf);
-				this.buffer = newBuf;
-			}
-		});
-		function write(dataType, size, value) {
-			var oldoffset = this.offset;
-			this.offset += size;
-			this.buffer['write' + dataType](value, oldoffset);
-			return this;
-		}
-
-		this[nbt.tagTypes.byte]   = write.bind(this, 'Int8', 1);
-		this[nbt.tagTypes.short]  = write.bind(this, 'Int16BE', 2);
-		this[nbt.tagTypes.int]    = write.bind(this, 'Int32BE', 4);
-		this[nbt.tagTypes.float]  = write.bind(this, 'FloatBE', 4);
-		this[nbt.tagTypes.double] = write.bind(this, 'DoubleBE', 8);
-
-		this[nbt.tagTypes.long] = function(value) {
-			this.int(value[0]);
-			this.int(value[1]);
-			return this;
-		};
-
-		this[nbt.tagTypes.byteArray] = function(value) {
-			this.int(value.length);
-			var oldoffset = this.offset;
-			this.offset += value.length;
-			value.copy(this.buffer, oldoffset);
-			return this;
-		};
-
-		this[nbt.tagTypes.intArray] = function(value) {
-			this.int(value.length);
-			var i;
-			for (i = 0; i < value.length; i++) {
-				this.int(value[i]);
-			}
-			return this;
-		};
-
-		this[nbt.tagTypes.string] = function(value) {
-			function byteLength(str) {
-				// returns the byte length of an utf8 string
-				var s = str.length;
-				for (var i=str.length-1; i>=0; i--) {
-					var code = str.charCodeAt(i);
-					if (code > 0x7f && code <= 0x7ff) s++;
-					else if (code > 0x7ff && code <= 0xffff) s+=2;
-					if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
-				}
-				return s;
-			}
-			var len = byteLength(value);
-			this.short(len);
-			var oldoffset = this.offset;
-			this.offset += len
-			this.buffer.write(value, oldoffset);
-			return this;
-		};
-
-		this[nbt.tagTypes.list] = function(value) {
-			this.byte(nbt.tagTypes[value.type]);
-			this.int(value.value.length);
-			var i;
-			for (i = 0; i < value.value.length; i++) {
-				this[value.type](value.value[i]);
-			}
-			return this;
-		};
-
-		this[nbt.tagTypes.compound] = function(value) {
-			var self = this;
-			Object.keys(value).map(function (key) {
-				self.byte(nbt.tagTypes[value[key].type]);
-				self.string(key);
-				self[value[key].type](value[key].value);
-			});
-			this.byte(nbt.tagTypes.end);
-			return this;
-		};
-
-		var typeName;
-		for (typeName in nbt.tagTypes) {
-			if (nbt.tagTypes.hasOwnProperty(typeName)) {
-				this[typeName] = this[nbt.tagTypes[typeName]];
-			}
-		}
-
-    }
-	nbt.Reader = function(buffer) {
-		this.offset = 0;
-
-		function read(dataType, size) {
-			var val = buffer['read' + dataType](this.offset);
-			this.offset += size;
-			return val;
-		}
-
-		this[nbt.tagTypes.byte]   = read.bind(this, 'Int8', 1);
-		this[nbt.tagTypes.short]  = read.bind(this, 'Int16BE', 2);
-		this[nbt.tagTypes.int]    = read.bind(this, 'Int32BE', 4);
-		this[nbt.tagTypes.float]  = read.bind(this, 'FloatBE', 4);
-		this[nbt.tagTypes.double] = read.bind(this, 'DoubleBE', 8);
-
-		this[nbt.tagTypes.long] = function() {
-			return [this.int(), this.int()];
-		};
-
-		this[nbt.tagTypes.byteArray] = function() {
-			var length = this.int();
-			var bytes = [];
-			var i;
-			for (i = 0; i < length; i++) {
-				bytes.push(this.byte());
-			}
-			return bytes;
-		};
-
-		this[nbt.tagTypes.intArray] = function() {
-			var length = this.int();
-			var ints = [];
-			var i;
-			for (i = 0; i < length; i++) {
-				ints.push(this.int());
-			}
-			return ints;
-		};
-
-		this[nbt.tagTypes.string] = function() {
-			var length = this.short();
-			var val = buffer.toString('utf8', this.offset, this.offset + length);
-			this.offset += length;
-			return val;
-		};
-
-		this[nbt.tagTypes.list] = function() {
-			var type = this.byte();
-			var length = this.int();
-			var values = [];
-			var i;
-			for (i = 0; i < length; i++) {
-				values.push(this[type]());
-			}
-			return { type: nbt.tagTypeNames[type], value: values };
-		};
-
-		this[nbt.tagTypes.compound] = function() {
-			var values = {};
-			while (true) {
-				var type = this.byte();
-				if (type === nbt.tagTypes.end) {
-					break;
-				}
-				var name = this.string();
-				var value = this[type]();
-				values[name] = { type: nbt.tagTypeNames[type], value: value };
-			}
-			return values;
-		};
-
-		var typeName;
-		for (typeName in nbt.tagTypes) {
-			if (nbt.tagTypes.hasOwnProperty(typeName)) {
-				this[typeName] = this[nbt.tagTypes[typeName]];
-			}
-		}
-	};
-
-	var writeUncompressed = this.writeUncompressed = function(value) {
-		var writer = new nbt.Writer();
-
-		writer.byte(nbt.tagTypes.compound);
-		writer.string(value.root);
-		writer.compound(value.value);
-		return writer.buffer;
-	}
-
-	var parseUncompressed = this.parseUncompressed = function(data) {
-		var buffer = new Buffer(data);
-		var reader = new nbt.Reader(buffer);
-
-		var type = reader.byte();
-		if (type !== nbt.tagTypes.compound) {
-			throw new Error('Top tag should be a compound');
-		}
-
-		var name = reader.string();
-		var value = reader.compound();
-
-		var result = { size: reader.offset, value: { root: name, value: value } };
-		return result;
-	}
-
-	this.parse = function(data, callback) {
-		if (hasGzipHeader(data)) {
-			zlib.gunzip(data, function(error, uncompressed) {
-				if (error) {
-					callback(error, data);
-				} else {
-					callback(null, parseUncompressed(uncompressed));
-				}
-			});
-		} else {
-			callback(null, parseUncompressed(data));
-		}
-	};
-}).apply(exports || (nbt = {}));
+module.exports={
+  writeUncompressed:writeUncompressed,
+  parseUncompressed:parseUncompressed,
+  parse:parse,
+  proto:proto
+};
--- test/nbt-spec.js
+++ test/nbt-spec.js
@@ -1,286 +1,77 @@
 'use strict';
 
-var fs = require('fs'),
-    nbt = require('../nbt'),
-    expect = require('chai').expect;
-
-describe('nbt.Writer', function() {
-	it('is constructed with no arguments', function() {
-		new nbt.Writer();
-	});
-
-	it('writes 8-bit bytes', function() {
-		var writer = new nbt.Writer();
-		expect(writer.byte(0).buffer.toString('hex')).to.deep.equal(new Buffer([0]).toString('hex'));
-		expect(writer.byte(127).buffer).to.deep.equal(new Buffer([0, 127]));
-		expect(writer.byte(-127).buffer).to.deep.equal(new Buffer([0, 127, -127]));
-	});
-
-	it('writes 16-bit shorts', function() {
-		var writer = new nbt.Writer();
-		expect(writer.short(0).buffer).to.deep.equal(new Buffer([0, 0]));
-		expect(writer.short(255).buffer).to.deep.equal(new Buffer([0, 0, 0, 255]));
-		expect(writer.short((-127 << 8) | 255).buffer).to.deep.equal(new Buffer([0, 0, 0, 255, -127, 255]));
-	});
-
-	it('writes 32-bit ints', function() {
-		var writer = new nbt.Writer();
-		expect(writer.int(0).buffer).to.deep.equal(new Buffer([0, 0, 0, 0]));
-		expect(writer.int(255).buffer).to.deep.equal(new Buffer([0, 0, 0, 0, 0, 0, 0, 255]));
-		expect(writer.int(-127 << 24).buffer).to.deep.equal(new Buffer([0, 0, 0, 0, 0, 0, 0, 255, -127, 0, 0, 0]));
-	});
-
-	it('writes 64-bit longs', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,255,
-			-127,0,0,0,0,0,0,0
-		]);
-		expect(writer.long([0, 0]).buffer).to.deep.equal(buffer.slice(0, 8));
-		expect(writer.long([0, 255]).buffer).to.deep.equal(buffer.slice(0, 16));
-		expect(writer.long([-127 << 24, 0]).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes 32-bit floats', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0x00,0x00,0x00,0x00,
-			0x3f,0x80,0x00,0x00
-		]);
-		expect(writer.float(0).buffer).to.deep.equal(buffer.slice(0, 4));
-		expect(writer.float(1).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes 64-bit doubles', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-			0x3f,0xf0,0x00,0x00,0x00,0x00,0x00,0x00
-		]);
-		expect(writer.double(0).buffer).to.deep.equal(buffer.slice(0, 8));
-		expect(writer.double(1).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes 8-bit byte arrays', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0,0,0,2, 1,2,
-			0,0,0,4, 3,4,5,6
-		]);
-		expect(writer.byteArray(new Buffer([1, 2])).buffer).to.deep.equal(buffer.slice(0, 6));
-		expect(writer.byteArray(new Buffer([3, 4, 5, 6])).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes 32-bit int arrays', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0,0,0,2, 0,0,0,1, 0,0,0,2,
-			0,0,0,4, 0,0,0,3, 0,0,0,4, 0,0,0,5, 0,0,0,6,
-		]);
-		expect(writer.intArray([1, 2]).buffer).to.deep.equal(buffer.slice(0, 12));
-		expect(writer.intArray([3, 4, 5, 6]).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes strings', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			0,6,  0x48,0x65,0x6C,0x6C,0x6F,0x21,
-			0,16, 0xE3,0x81,0x93,0xE3,0x82,0x93,0xE3,0x81,
-			      0xAB,0xE3,0x81,0xA1,0xE3,0x81,0xAF,0x21
-		]);
-		expect(writer.string('Hello!').buffer).to.deep.equal(buffer.slice(0, 8));
-		expect(writer.string('こんにちは!').buffer).to.deep.equal(buffer);
-	});
-
-	it('writes lists', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			1, 0,0,0,3, 1, 2, 3,
-			8, 0,0,0,2, 0,5, 0x48,0x65,0x6C,0x6C,0x6F,
-			            0,5, 0x57,0x6F,0x72,0x6C,0x64
-		]);
-		expect(writer.list({ type: "byte", value: [1, 2, 3] }).buffer).to.deep.equal(buffer.slice(0, 8));
-		expect(writer.list({ type: "string", value: ['Hello', 'World'] }).buffer).to.deep.equal(buffer);
-	});
-
-	it('writes compounds', function() {
-		var writer = new nbt.Writer();
-		var buffer = new Buffer([
-			1, 0,2, 0x61,0x61, 1,
-			9, 0,2, 0x62,0x62, 1, 0,0,0,3, 1, 2, 3,
-			0,
-			1, 0,2, 0x63,0x63, 2,
-			0
-		]);
-		expect(writer.compound({
-			aa: { type: "byte", value: 1 },
-			bb: { type: "list", value: { type: "byte", value: [1, 2, 3] } }
-		}).buffer.toString('hex')).to.deep.equal(buffer.slice(0, 20).toString('hex'));
-		expect(writer.compound({
-			cc: { type: "byte", value: 2 }
-		}).buffer).to.deep.equal(buffer);
-	});
-});
-
-describe('nbt.Reader', function() {
-	it('is constructed with a buffer array', function() {
-		new nbt.Reader(new Buffer([1, 2, 3]));
-	});
-
-	it('reads 8-bit bytes', function() {
-		var reader = new nbt.Reader(new Buffer([0, 127, -127]));
-		expect(reader.byte()).to.equal(0);
-		expect(reader.byte()).to.equal(127);
-		expect(reader.byte()).to.equal(-127);
-	});
-
-	it('reads 16-bit shorts', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,0, 0,255, -127,255
-		]));
-		expect(reader.short()).to.equal(0);
-		expect(reader.short()).to.equal(255);
-		expect(reader.short()).to.equal((-127 << 8) | 255);
-	});
-
-	it('reads 32-bit ints', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,0,0,0,
-			0,0,0,255,
-			-127,0,0,0
-		]));
-		expect(reader.int()).to.equal(0);
-		expect(reader.int()).to.equal(255);
-		expect(reader.int()).to.equal(-127 << 24);
-	});
-
-	it('reads 64-bit longs', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,255,
-			-127,0,0,0,0,0,0,0
-		]));
-		expect(reader.long()).to.deep.equal([0, 0]);
-		expect(reader.long()).to.deep.equal([0, 255]);
-		expect(reader.long()).to.deep.equal([-127 << 24, 0]);
-	});
-
-	it('reads 32-bit floats', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0x00,0x00,0x00,0x00,
-			0x3f,0x80,0x00,0x00
-		]));
-		expect(reader.float()).to.equal(0);
-		expect(reader.float()).to.equal(1);
-	});
-
-	it('reads 64-bit doubles', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-			0x3f,0xf0,0x00,0x00,0x00,0x00,0x00,0x00
-		]));
-		expect(reader.double()).to.equal(0);
-		expect(reader.double()).to.equal(1);
-	});
-
-	it('reads 8-bit byte arrays', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,0,0,2, 1,2,
-			0,0,0,4, 3,4,5,6
-		]));
-		expect(reader.byteArray()).to.deep.equal([1, 2]);
-		expect(reader.byteArray()).to.deep.equal([3, 4, 5, 6]);
-	});
-
-	it('reads 32-bit int arrays', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,0,0,2, 0,0,0,1, 0,0,0,2,
-			0,0,0,4, 0,0,0,3, 0,0,0,4, 0,0,0,5, 0,0,0,6,
-		]));
-		expect(reader.intArray()).to.deep.equal([1, 2]);
-		expect(reader.intArray()).to.deep.equal([3, 4, 5, 6]);
-	});
-
-	it('reads strings', function() {
-		var reader = new nbt.Reader(new Buffer([
-			0,6,  0x48,0x65,0x6C,0x6C,0x6F,0x21,
-			0,16, 0xE3,0x81,0x93,0xE3,0x82,0x93,0xE3,0x81,
-			      0xAB,0xE3,0x81,0xA1,0xE3,0x81,0xAF,0x21
-		]));
-		expect(reader.string()).to.equal('Hello!');
-		expect(reader.string()).to.equal('こんにちは!');
-	});
-
-	it('reads lists', function() {
-		var reader = new nbt.Reader(new Buffer([
-			1, 0,0,0,3, 1, 2, 3,
-			8, 0,0,0,2, 0,5, 0x48,0x65,0x6C,0x6C,0x6F,
-			            0,5, 0x57,0x6F,0x72,0x6C,0x64
-		]));
-		expect(reader.list()).to.deep.equal({ type: "byte", value: [1, 2, 3] });
-		expect(reader.list()).to.deep.equal({ type: "string", value: ['Hello', 'World'] });
-	});
-
-	it('reads compounds', function() {
-		var reader = new nbt.Reader(new Buffer([
-			1, 0,2, 0x61,0x61, 1,
-			9, 0,2, 0x62,0x62, 1, 0,0,0,3, 1, 2, 3,
-			0,
-			1, 0,2, 0x63,0x63, 2,
-			0
-		]));
-		expect(reader.compound()).to.deep.equal({
-			aa: { type: "byte", value: 1 },
-			bb: { type: "list", value: { type: "byte", value: [1, 2, 3] } }
-		});
-		expect(reader.compound()).to.deep.equal({
-			cc: { type: "byte", value: 2 }
-		});
-	});
-});
+var fs = require('fs');
+var nbt = require('../nbt');
+var expect = require('chai').expect;
 
 describe('nbt.parse', function() {
-	it('parses a compressed NBT file', function(done) {
-		fs.readFile('sample/bigtest.nbt.gz', function(error, data) {
-			if (error) {
-				throw error;
-			}
-			nbt.parse(data, function(err, data) {
-				if (err)
-					throw error;
-				expect(data.value.root).to.equal('Level');
-				expect(data.value.value.stringTest.value).to.equal(
-				'HELLO WORLD THIS IS A TEST STRING ÅÄÖ!');
-				expect(data.value.value['nested compound test'].value).to.deep.equal({
-					ham: {
-						type: "compound",
-						value: {
-							name: { type: "string", value: "Hampus" },
-							value: { type: "float", value: 0.75 }
-						}
-					},
-					egg: {
-						type: "compound",
-						value: {
-							name: { type: "string", value: 'Eggbert' },
-							value: { type: "float", value: 0.5 }
-						}
-					}
-				});
-				done();
-			});
-		});
-	});
+  function checkBigtest(data) {
+    expect(data.value.stringTest.value).to.equal(
+      'HELLO WORLD THIS IS A TEST STRING ÅÄÖ!');
+    expect(data.value['nested compound test'].value).to.deep.equal({
+      ham: {
+        type: "compound",
+        value: {
+          name: { type: "string", value: "Hampus" },
+          value: { type: "float", value: 0.75 }
+        }
+      },
+      egg: {
+        type: "compound",
+        value: {
+          name: { type: "string", value: 'Eggbert' },
+          value: { type: "float", value: 0.5 }
+        }
+      }
+    });
+  }
+
+  it('parses a compressed NBT file', function(done) {
+    fs.readFile('sample/bigtest.nbt.gz', function(error, data) {
+      if (error) {
+        throw error;
+      }
+      nbt.parse(data, function(err, data) {
+        if (err) {
+          throw error;
+        }
+        checkBigtest(data);
+        done();
+      });
+    });
+  });
+
+  it('parses an uncompressed NBT file through parse()', function(done) {
+    fs.readFile('sample/bigtest.nbt', function(error, data) {
+      if (error) {
+        throw error;
+      }
+      nbt.parse(data, function(error, data) {
+        if (error) {
+          throw error;
+        }
+        checkBigtest(data);
+        done();
+      });
+    });
+  });
 });
 
 describe('nbt.write', function() {
-	it('writes an uncompressed NBT file', function(done) {
-		fs.readFile('sample/bigtest.nbt', function(err, nbtdata) {
-			if (err)
-				throw err;
-			expect(nbt.writeUncompressed(require('../sample/bigtest'))).to.deep.equal(nbtdata);
-			done();
-		});
-	});
+  it('writes an uncompressed NBT file', function(done) {
+    fs.readFile('sample/bigtest.nbt', function(err, nbtdata) {
+      if (err) {
+        throw err;
+      }
+      expect(nbt.writeUncompressed(require('../sample/bigtest'))).to.deep.equal(nbtdata);
+      done();
+    });
+  });
+
+  it('re-encodes it input perfectly', function() {
+    var input = require('../sample/bigtest');
+    var output = nbt.writeUncompressed(input);
+    var decodedOutput = nbt.parseUncompressed(output);
+    expect(decodedOutput).to.deep.equal(input);
+  });
 });
