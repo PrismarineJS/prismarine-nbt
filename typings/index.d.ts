f0cb610b8ebdee2cafa8ec6d4707ef4880ed4644
McPqndq <kellybob3737@gmail.com>
2020-10-02 14:07:38 -0500

    export list type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,10 @@
 
 declare module 'prismarine-nbt'{
-  type List<T extends TagType> = {type: TagType.List, value: {type: Tags[T]['type'], value: Tags[T]['value'][]}};
+  export type List<T extends TagType> = {
+    type: TagType.List,
+    value: { type: Tags[T]['type'], value: Tags[T]['value'][] }
+  };
+
   export enum TagType {
     Byte = 'byte',
     Short = 'short',
@@ -15,21 +19,24 @@ declare module 'prismarine-nbt'{
     IntArray = 'intArray',
     LongArray = 'longArray',
   }
+
   export type Tags = {
-    [TagType.Byte]: {type: TagType.Byte, value: number};
-    [TagType.Short]: {type: TagType.Short, value: number};
-    [TagType.Int]: {type: TagType.Int, value: number};
-    [TagType.Long]: {type: TagType.Long, value: [number, number]};
-    [TagType.Float]: {type: TagType.Float, value: number};
-    [TagType.Double]: {type: TagType.Double, value: number};
-    [TagType.ByteArray]: {type: TagType.ByteArray, value: number[]};
-    [TagType.String]: {type: TagType.String, value: string};
+    [TagType.Byte]: { type: TagType.Byte, value: number };
+    [TagType.Short]: { type: TagType.Short, value: number };
+    [TagType.Int]: { type: TagType.Int, value: number };
+    [TagType.Long]: { type: TagType.Long, value: [number, number] };
+    [TagType.Float]: { type: TagType.Float, value: number };
+    [TagType.Double]: { type: TagType.Double, value: number };
+    [TagType.ByteArray]: { type: TagType.ByteArray, value: number[] };
+    [TagType.String]: { type: TagType.String, value: string };
     [TagType.List]: List<TagType>
-    [TagType.Compound]: {type: TagType.Compound, value: {[x in string]?: Tags[TagType]}};
-    [TagType.IntArray]: {type: TagType.IntArray, value: number[]};
-    [TagType.LongArray]: {type: TagType.LongArray, value: [number, number][]};
+    [TagType.Compound]: { type: TagType.Compound, value: Recorc<string, Tags[TagType]> };
+    [TagType.IntArray]: { type: TagType.IntArray, value: number[] };
+    [TagType.LongArray]: { type: TagType.LongArray, value: [number, number][] };
   }
+
   export type NBT = Tags['compound'] & {name: string};
+
   export function writeUncompressed(value: NBT, le?: boolean): ArrayBuffer;
   export function parseUncompressed(value: ArrayBuffer, le?: boolean): NBT;
   export function parse(data: ArrayBuffer, le:boolean, callback: (err: Error | null, value: NBT) => any): void;
