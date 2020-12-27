2a8f447f6377414018f5f2ee94cc3df001aad4f0
extremeheat <extreme@protonmail.ch>
2020-12-27 09:07:57 -0500

    support LE varint nbt, nbt format detection, tagname sanity checks

---
+++ compiler-zigzag.js
@@ -0,0 +1,55 @@
+/* global PartialReadError */
+function sizeOfVarInt (value) {
+  if (typeof value !== 'bigint') value = BigInt(value)
+  let cursor = 0
+  while (value & ~0x7Fn) {
+    value >>= 7n
+    cursor++
+  }
+  return cursor + 1
+}
+
+function readSignedVarInt (buffer, offset) {
+  let result = BigInt(0)
+  let shift = 0
+  let cursor = offset
+  let size = 0
+
+  while (true) {
+    if (cursor + 1 > buffer.length) { throw new PartialReadError() }
+    const b = buffer.readUInt8(cursor)
+    result |= BigInt((b & 0x7f) << shift) // Add the bits to our number, except MSB
+    cursor++
+    if (!(b & 0x80)) { // If the MSB is not set, we return the number
+      size = cursor - offset
+      break
+    }
+    shift += 7 // we only have 7 bits, MSB being the return-trigger
+    if (shift > 63) throw new PartialReadError(`varint is too big: ${shift}`)
+  }
+
+  const unsigned = result
+  const zigzag = unsigned >> 1n
+  const value = zigzag ^ (unsigned & (1n << 63n))
+  return { value, size }
+}
+
+function writeSignedVarInt (value, buffer, offset) {
+  if (typeof value !== 'bigint') value = BigInt(value)
+  value = (value << 1n) ^ (value >> 31n)
+  let cursor = 0
+  while (value & ~0x7Fn) {
+    const num = Number((value & 0xFFn) | 0x80n)
+    buffer.writeUInt8(num, offset + cursor)
+    cursor++
+    value >>= 7n
+  }
+  buffer.writeUInt8(Number(value), offset + cursor)
+  return offset + cursor + 1
+}
+
+module.exports = {
+  Read: { zigzag: ['context', readSignedVarInt] },
+  Write: { zigzag: ['context', writeSignedVarInt] },
+  SizeOf: { zigzag: ['context', sizeOfVarInt] }
+}
--- nbt.js
+++ nbt.js
@@ -2,25 +2,46 @@ const zlib = require('zlib')
 
 const { ProtoDefCompiler } = require('protodef').Compiler
 
-const nbtJson = JSON.stringify(require('./nbt.json'))
-const leNbtJson = nbtJson.replace(/(i[0-9]+)/g, 'l$1')
+const beNbtJson = JSON.stringify(require('./nbt.json'))
+const leNbtJson = beNbtJson.replace(/([if][0-7]+)/g, 'l$1')
+const varintJson = JSON.stringify(require('./nbt-varint.json')).replace(/([if][0-9]+)/g, 'l$1')
 
-function createProto (le) {
+function createProto (type) {
   const compiler = new ProtoDefCompiler()
   compiler.addTypes(require('./compiler-compound'))
-  compiler.addTypesToCompile(JSON.parse(le ? leNbtJson : nbtJson))
+  compiler.addTypes(require('./compiler-tagname'))
+  let proto = beNbtJson
+  if (type === 'little-varint') {
+    compiler.addTypes(require('./compiler-zigzag'))
+    proto = varintJson
+  } else if (type === 'little') {
+    proto = leNbtJson
+  }
+  compiler.addTypesToCompile(JSON.parse(proto))
   return compiler.compileProtoDefSync()
 }
 
-const proto = createProto(false)
-const protoLE = createProto(true)
+const protoBE = createProto('big')
+const protoLE = createProto('little')
+const protoVarInt = createProto('little-varint')
 
-function writeUncompressed (value, le) {
-  return (le ? protoLE : proto).createPacketBuffer('nbt', value)
+const protos = {
+  big: protoBE,
+  little: protoLE,
+  'little-varint': protoVarInt
 }
 
-function parseUncompressed (data, le) {
-  return (le ? protoLE : proto).parsePacketBuffer('nbt', data).data
+function writeUncompressed (value, proto = 'big') {
+  if (proto === true) proto = 'little'
+  proto = proto === true ? proto = 'little' : proto
+  // console.log('Creating uncompressed', value, proto)
+  return protos[proto].createPacketBuffer('nbt', value)
+}
+
+function parseUncompressed (data, proto = 'big') {
+  if (proto === true) proto = 'little'
+  // console.log('Reading uncompressed', data, proto)
+  return protos[proto].parsePacketBuffer('nbt', data, data.startOffset)
 }
 
 const hasGzipHeader = function (data) {
@@ -30,24 +51,97 @@ const hasGzipHeader = function (data) {
   return result
 }
 
-function parse (data, le, callback) {
-  let isLe = false
-  if (typeof le === 'function') {
-    callback = le
-  } else {
-    isLe = le
+function parseAs (data, type) {
+  return new Promise((resolve, reject) => {
+    if (hasGzipHeader(data)) {
+      zlib.gunzip(data, function (error, uncompressed) {
+        if (error) {
+          reject(error)
+        } else {
+          const ret = parseUncompressed(uncompressed, type)
+          resolve([null, ret.data, type, ret.metadata])
+        }
+      })
+    } else {
+      const ret = parseUncompressed(data, type)
+      resolve([null, ret.data, type, ret.metadata])
+    }
+  })
+
+  // We can't execute callback here, otherwise errors
+  // in the callback will break fallback detection
+}
+
+async function parse (data, format, callback) {
+  // console.log('A0', data, format, callback)
+
+  let fmt = null
+  if (typeof format === 'function') {
+    callback = format
+  } else if (format === true || format === 'little') {
+    fmt = 'little'
+  } else if (format === 'big') {
+    fmt = 'big'
+  } else if (format === 'little-varint') {
+    fmt = 'little-varint'
+  } else if (format) {
+    throw new Error('Unrecognized format: ' + format)
   }
-  if (hasGzipHeader(data)) {
-    zlib.gunzip(data, function (error, uncompressed) {
-      if (error) {
-        callback(error, data)
-      } else {
-        callback(null, parseUncompressed(uncompressed, isLe))
+  if (!callback) callback = () => {}
+
+  if (fmt) {
+    // console.log('Read as', fmt)
+    return parseAs(data, fmt).then(callback)
+  }
+
+  // let didCallback = false
+  // const callbackOnce = (args) => {
+  //   // if (!didCallback) callback(...args)
+  //   didCallback = true
+  // }
+
+  // console.log('Parsing')
+  // return parseAs(data, 'big')
+  //   .catch(e0 => parseAs(data, 'little-varint')
+  //     .catch(e1 => parseAs(data, 'little')
+  //       .catch(e2 => {
+  //         console.warn('Failed to read as big endian');
+  //         throw e0 // return first error reading as BE
+  //       }).then(callbackOnce)
+  //     ).then(callbackOnce)
+  //   ).then(callbackOnce)
+  //   .finally('SHOULD GET CALLED!')
+
+  let ret = null
+  try {
+    ret = await parseAs(data, 'big')
+  } catch (e) {
+    // console.debug('Failed read as big endian, trying le')
+    try {
+      ret = await parseAs(data, 'little')
+    } catch (e2) {
+      // console.debug('Failed read as le varint, trying le')
+      try {
+        ret = await parseAs(data, 'little-varint')
+      } catch (e3) {
+        console.warn('Failed to read nbt')
+        console.log(e)
+        throw e // error decoding as big endian
       }
-    })
-  } else {
-    callback(null, parseUncompressed(data, isLe))
+    }
   }
+
+  if (ret[0]) {
+    throw new Error(ret[0])
+  }
+
+  callback(...ret) // eslint-disable-line
+  const [, result, type, metadata] = ret
+  return { result, type, metadata }
+}
+
+function parseAsync (data, format) {
+  return new Promise(resolve => parse(data, format, (...args) => resolve(args)))
 }
 
 function simplify (data) {
@@ -71,6 +165,9 @@ module.exports = {
   parseUncompressed,
   simplify,
   parse,
-  proto,
-  protoLE
+  parseAs,
+  parseAsync,
+  proto: protoBE,
+  protoLE,
+  protos
 }
