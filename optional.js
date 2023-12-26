9e9387e2b86eea2ab2d7458708ec0812dd1eb681
extremeheat <extreme@protonmail.ch>
2023-12-26 03:49:32 +0000

    fix optional compiler code

--- optional.js
+++ optional.js
@@ -20,33 +20,30 @@ function sizeOfOptionalNbt (value, { tagType }, rootNode) {
 const compiler = {
   Read: {
     optionalNbtType: ['parametrizable', (compiler, { tagType }) => {
-      const code = `
-      if (offset + 1 > buffer.length) { throw new PartialReadError() }
-      if (buffer.readInt8(offset) === 0) return { size: 1 }
-      return ${compiler.callType(tagType)}
-      `
-      return compiler.wrapCode(code)
+      return compiler.wrapCode(`
+if (offset + 1 > buffer.length) { throw new PartialReadError() }
+if (buffer.readInt8(offset) === 0) return { size: 1 }
+return ${compiler.callType(tagType)}
+      `)
     }]
   },
   Write: {
     optionalNbtType: ['parametrizable', (compiler, { tagType }) => {
-      const code = `
-      if (value === undefined) {
-        buffer.writeInt8(0, offset)
-        return offset + 1
-      }
-      return ${compiler.callType(tagType)}
-      `
-      return compiler.wrapCode(code)
+      return compiler.wrapCode(`
+if (value === undefined) {
+  buffer.writeInt8(0, offset)
+  return offset + 1
+}
+return ${compiler.callType('value', tagType)}
+      `)
     }]
   },
   SizeOf: {
     optionalNbtType: ['parametrizable', (compiler, { tagType }) => {
-      const code = `
-      if (value === undefined) { return 1 }
-      return ${compiler.callType(tagType)}
-      `
-      return compiler.wrapCode(code)
+      return compiler.wrapCode(`
+if (value === undefined) { return 1 }
+return ${compiler.callType('value', tagType)}
+      `)
     }]
   }
 }
