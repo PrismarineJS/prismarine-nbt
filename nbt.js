1066ce273f7e27fb20fb6ec471ef549931338711
bdkopen <bdkopen@gmail.com>
2024-09-06 21:27:08 -0400

    Revert "feat: use `skipChecks` flag to allow full NBT array size support"
    This reverts commit 6457edb5640fcb7204cae480b3a5cc8b100ce56c.

--- nbt.js
+++ nbt.js
@@ -35,7 +35,7 @@ function addTypesToInterpreter (type, compiler) {
 }
 
 function createProto (type) {
-  const compiler = new ProtoDefCompiler({ skipChecks: true })
+  const compiler = new ProtoDefCompiler()
   addTypesToCompiler(type, compiler)
   return compiler.compileProtoDefSync()
 }
